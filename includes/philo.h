40888b1a2b891b249af5da23cfbbb518c0a9f166
joeow06 <joeow06@gmail.com>
2025-03-13 13:59:26 +0800

    feat: added enum for custom mutex function

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: jow <jow@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:24:05 by jow               #+#    #+#             */
-/*   Updated: 2025/02/06 13:26:00 by jow              ###   ########.fr       */
+/*   Updated: 2025/03/13 13:55:06 by jow              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,36 +19,55 @@
 # include <pthread.h>
 # include <sys/time.h>
 
+typedef enum e_mutex
+{
+	INIT,
+	DESTROY,
+	LOCK,
+	UNLOCK,
+
+}	t_mutex_type;
+
 typedef struct s_philo
 {
 	int				id;
 	int				left_fork;
 	int				right_fork;
 	int				meals;
-	pthread_t		thread;
-	struct s_data	*data;
+	pthread_t		ph_mtx;
+	t_table			*table;
 }					t_philo;
 
 typedef struct s_table
 {
 	int				philo_count;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
+	time_t			time_to_die;
+	time_t			time_to_eat;
+	time_t			time_to_sleep;
+	time_t			start_time;
+	time_t			end_time;
 	int				meals;
 	int				dead_flag;
-	pthread_mutex_t	print;
-	pthread_mutex_t	dead_mutex;
-	pthread_mutex_t	meals_mutex;
+	pthread_mutex_t	access_mutex;
+	pthread_mutex_t	write_mutex;
 	pthread_mutex_t	*forks;
-	t_philo			*philos;
+	t_philo			*philo;
 }					t_table;
 
-/* CHECK_ARGS_C */
+/* PARSER_C */
 int		check_number(char *str);
 int		check_valid_args(int ac, char **av);
+void	parse_args(int ac, char **av, t_table *table);
+void	parser(int ac, char **av, t_table *table);
 
 /*	UTILS_C */
 int		ft_atoi(const char *str);
+
+/*	ERROR_C */
+void	print_error_str(char *str);
+
+/*	INIT_C */
+void init(int ac, char **av, t_table *table);
+void init_philo(t_table *table);
 
 #endif
