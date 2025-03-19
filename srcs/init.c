c175ce79ddecb5e5a4d85ed3bd600661fc5d3b03
joeow06 <joeow06@gmail.com>
2025-03-19 15:53:44 +0800

    fix: seg fault in ft_mutex fixed

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: jow <jow@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 13:26:18 by jow               #+#    #+#             */
-/*   Updated: 2025/03/13 13:57:15 by jow              ###   ########.fr       */
+/*   Updated: 2025/03/19 15:53:02 by jow              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,27 +26,25 @@ void init_philo(t_table *table)
 	int	i;
 	t_philo	*philo;
 
-	i = 0;
-	philo = malloc(sizeof(t_philo) * table->philo_count);
+	table->philo = malloc(sizeof(t_philo) * table->philo_count);
 	if (!table->philo)
 		print_error_str("Malloc failed");
+	i = 0;
 	while (i < table->philo_count)
 	{
 		philo = table->philo + i;
-		table->philo[i].id = i + 1;
-		table->philo[i].meals = 0;
+		philo->id = i + 1;
+		philo->meals = 0;
 		philo->table = table;
-		ft_mutex(&table->philo->ph_mtx, INIT);
-		assign_forks(philo, i);
+		ft_mutex(&philo->ph_mtx, INIT);
+		//assign_forks(philo, i);
 		i++;
 	}
-
+	printf("Done\n");
 }
 
 void init(int ac, char **av, t_table *table)
 {
-	if (!table)
-		print_error_str("Malloc failed");
 	table->philo_count = ft_atoi(av[1]);
 	table->time_to_die = ft_atoi(av[2]);
 	table->time_to_eat = ft_atoi(av[3]);
