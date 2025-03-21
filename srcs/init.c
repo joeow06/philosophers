c3fe4d422f6b0ad3ad683b742cc585a2a2631428
joeow06 <joeow06@gmail.com>
2025-03-21 17:31:03 +0800

    fix: correctly assign forks in a circle

--- srcs/init.c
+++ srcs/init.c
@@ -6,29 +6,42 @@
 /*   By: jow <jow@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 13:26:18 by jow               #+#    #+#             */
-/*   Updated: 2025/03/19 15:53:02 by jow              ###   ########.fr       */
+/*   Updated: 2025/03/21 17:07:12 by jow              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	assign_forks(t_philo *philo, int i)
+static void	assign_forks(t_philo *philo, t_fork *forks, int i)
 {
-	philo->left_fork = i;
-	if (i == philo->table->philo_count - 1)
-		philo->right_fork = 0;
-	else
-		philo->right_fork = i + 1;
+	philo->right_fork = &forks[i];
+	philo->left_fork = &forks[(i + 1) % philo->table->philo_count];
 }
 
-void init_philo(t_table *table)
+static void	init_forks(t_table *table)
 {
 	int	i;
+
+	table->forks = malloc(sizeof(t_fork) * table->philo_count);
+	if (!table->forks)
+		print_error_exit("Malloc failed");
+	i = 0;
+	while (i < table->philo_count)
+	{
+		ft_mutex(&table->forks[i].fork_mtx, INIT);
+		table->forks[i].fork_id = i;
+		i++;
+	}
+}
+
+static void	init_philo(t_table *table)
+{
+	int		i;
 	t_philo	*philo;
 
 	table->philo = malloc(sizeof(t_philo) * table->philo_count);
 	if (!table->philo)
-		print_error_str("Malloc failed");
+		print_error_exit("Malloc failed");
 	i = 0;
 	while (i < table->philo_count)
 	{
@@ -37,13 +50,12 @@ void init_philo(t_table *table)
 		philo->meals = 0;
 		philo->table = table;
 		ft_mutex(&philo->ph_mtx, INIT);
-		//assign_forks(philo, i);
+		assign_forks(philo, table->forks, i);
 		i++;
 	}
-	printf("Done\n");
 }
 
-void init(int ac, char **av, t_table *table)
+void	init(int ac, char **av, t_table *table)
 {
 	table->philo_count = ft_atoi(av[1]);
 	table->time_to_die = ft_atoi(av[2]);
@@ -53,6 +65,6 @@ void init(int ac, char **av, t_table *table)
 		table->meals = ft_atoi(av[5]);
 	else
 		table->meals = -1;
+	init_forks(table);
 	init_philo(table);
-	//init_forks;
 }
