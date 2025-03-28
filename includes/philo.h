0c0a3b2b84e56d283c3c3138cb3692cf550159e1
joeow06 <joeow06@gmail.com>
2025-03-28 15:13:04 +0800

    chore: added new items - ansi colour codes - enum list of status - added new variables - added new functions

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: jow <jow@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:24:05 by jow               #+#    #+#             */
-/*   Updated: 2025/03/24 00:24:56 by jow              ###   ########.fr       */
+/*   Updated: 2025/03/28 15:04:30 by jow              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,20 @@
 # include <pthread.h>
 # include <sys/time.h>
 
+# define RESET	"\033[0m"
+# define RED	"\033[31m"
+# define GREEN	"\033[32m"
+# define YELLOW	"\033[33m"
+# define BLUE	"\033[34m"
+# define CYAN	"\033[36m"
+# define MAGEN	"\033[35m"
+# define RESET	"\033[0m"
+
 typedef struct s_table	t_table;
 
 typedef enum e_thread_type
 {
-	INIT,
+	CREATE,
 	JOIN,
 	DETACH
 }	t_thread_type;
@@ -35,10 +44,17 @@ typedef enum e_mutex_type
 	DESTROY,
 	LOCK,
 	UNLOCK,
-	JOIN,
-	CREATE,
-	DETACH
 }	t_mtx_type;
+
+typedef enum e_status
+{
+	GOT_LEFT_FORK,
+	GOT_RIGHT_FORK,
+	THINKING,
+	SLEEPING,
+	EATING,
+	DIED
+}	t_status;
 
 typedef struct s_fork
 {
@@ -61,14 +77,15 @@ typedef struct s_philo
 typedef struct s_table
 {
 	int				philo_count;
+	int				meals;
 	time_t			time_to_die;
 	time_t			time_to_eat;
 	time_t			time_to_sleep;
 	time_t			start_time;
-	time_t			end_time;
-	int				meals;
 	bool			is_exit;
-	pthread_t		dead_thread;
+	bool			is_dead;
+	int				active_philo;
+	pthread_t		death_thread;
 	pthread_mutex_t	read_mutex;
 	pthread_mutex_t	write_mutex;
 	t_fork			*forks;
@@ -84,9 +101,13 @@ void	parser(int ac, char **av, t_table *table);
 /*	UTILS_C */
 int		ft_atoi(const char *str);
 time_t	get_time_in_ms(void);
+void	ft_wait(time_t time, t_table *table);
+void	active_philo(t_table *table);
 
 /*	PTHREAD_UTILS_C*/
 void	ft_mutex(pthread_mutex_t *mutex, t_mtx_type type);
+void	ft_thread(pthread_t *thread, void *(*func)(void *), void *arg, \
+		t_thread_type type);
 
 /*	ERROR_C */
 void	print_error_exit(char *str);
@@ -100,7 +121,14 @@ void	simulation(t_table *table);
 /*	SET_GET_C */
 void	set_bool(pthread_mutex_t *mutex, bool *value, bool new_value);
 bool	get_bool(pthread_mutex_t *mutex, bool *value);
-void	set_long(pthread_mutex_t *mutex, long *value, long new_value);
-long	get_long(pthread_mutex_t *mutex, long *value);
+void	set_time(pthread_mutex_t *mutex, time_t *value, time_t new_value);
+long	get_time(pthread_mutex_t *mutex, time_t *value);
+
+/*	PRINT_C */
+void	print_status(t_philo *philo, t_status status);
+
+/*	MONITOR_C */
+void	*monitoring(void *data);
+
 
 #endif
