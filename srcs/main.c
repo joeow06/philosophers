6a84b8282526e8d8d8bd73cde28b4eb84302e2c4
joeow06 <joeow06@gmail.com>
2025-03-30 15:05:41 +0800

    fix: norm fix and commented out print_data

--- srcs/main.c
+++ srcs/main.c
@@ -6,30 +6,30 @@
 /*   By: jow <jow@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:19:51 by jow               #+#    #+#             */
-/*   Updated: 2025/03/25 18:35:19 by jow              ###   ########.fr       */
+/*   Updated: 2025/03/30 14:10:11 by jow              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	print_fork_assignments(t_table *table)
+void	print_data(t_table *table);
+
+int	main(int ac, char **av)
 {
-	int i;
+	t_table	table;
 
-	printf("\n===== Philosopher Fork Assignments =====\n");
-	for (i = 0; i < table->philo_count; i++)
+	if (ac != 5 && ac != 6)
 	{
-		t_philo *philo = &table->philo[i];
-
-		printf("Philosopher %d:\n", philo->id);
-		printf("  Left Fork Address:  %p (Fork ID: %d)\n",
-			   (void *)philo->left_fork, philo->left_fork->fork_id);
-		printf("  Right Fork Address: %p (Fork ID: %d)\n",
-			   (void *)philo->right_fork, philo->right_fork->fork_id);
-		printf("--------------------------------------\n");
+		printf("Error: Wrong number of arguments\n");
+		return (1);
 	}
+	parser(ac, av, &table);
+	init(ac, av, &table);
+	simulation(&table);
+	free_table(&table);
+	return (0);
 }
-
+/*
 void	print_data(t_table *table)
 {
 	int i;
@@ -55,21 +55,4 @@ void	print_data(t_table *table)
 		i++;
 	}
 }
-
-int	main(int ac, char **av)
-{
-	t_table	table;
-
-	if (ac != 5 && ac != 6)
-	{
-		printf("Error: Wrong number of arguments\n");
-		return (1);
-	}
-	parser(ac, av, &table);
-	init(ac, av, &table);
-	printf("Simulation started\n");
-	//print_fork_assignments(&table);
-	print_data(&table);
-	simulation(&table);
-	return (0);
-}
+*/
