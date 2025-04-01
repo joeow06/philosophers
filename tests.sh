618608a9d6bb9c5784cd69103d5710bb9ecffebd
joeow06 <joeow06@gmail.com>
2025-04-02 00:11:35 +0800

    chore: added more tests

--- tests.sh
+++ tests.sh
@@ -1,6 +1,16 @@
 make
 make clean
 
+echo "--------PARAMETERS TESTS--------"
+./philo 8a 100 100 50
+./philo 5 -100 200 200
+./philo 5 100 100 -50
+./philo five 800 200 200
+./philo 5 800 200 200 6 8
+./philo 0 800 200 200
+./philo 3 800 200
+./philo 3 800 abc 200
+
 echo "\na philo should die\n"
 ./philo 1 500 200 100
 echo "----------------------------------"
