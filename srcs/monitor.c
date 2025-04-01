265403243ccbf3d857000aa5e87726db2400cf26
joeow06 <joeow06@gmail.com>
2025-04-01 22:56:47 +0800

    refactor: removed unecessary boolean declaration

--- srcs/monitor.c
+++ srcs/monitor.c
@@ -6,7 +6,7 @@
 /*   By: jow <jow@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 20:12:02 by jow               #+#    #+#             */
-/*   Updated: 2025/03/31 16:53:00 by jow              ###   ########.fr       */
+/*   Updated: 2025/04/01 22:46:09 by jow              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,21 @@ static bool	wait_active_philo(t_table *table);
 
 static bool	all_philo_full(t_table *table)
 {
-	bool	value;
 	int		i;
 
-	value = true;
 	i = 0;
 	while (i < table->philo_count)
 	{
 		ft_mutex(&table->philo[i].ph_mtx, LOCK);
-		if (table->philo[i].meals > 0 || table->philo[i].meals < 0)
-			value = false;
+		if (table->philo[i].meals != 0)
+		{
+			ft_mutex(&table->philo[i].ph_mtx, UNLOCK);
+			return (false);
+		}
 		ft_mutex(&table->philo[i].ph_mtx, UNLOCK);
-		if (value == false)
-			break ;
 		i++;
 	}
-	return (value);
+	return (true);
 }
 
 /*
@@ -47,10 +46,16 @@ static bool	is_philo_dead(t_philo *philo)
 	time_t	last_meal;
 
 	last_meal = get_time(&philo->ph_mtx, &philo->last_meal_time);
-	if (last_meal + philo->table->time_to_die < get_time_in_ms())
-		set_bool(&philo->ph_mtx, &philo->table->is_dead, true);
+	ft_mutex(&philo->ph_mtx, LOCK);
+	if (last_meal + philo->table->time_to_die < get_time_in_ms() && \
+		philo->meals != 0)
+	{
+		set_bool(&philo->table->read_mutex, &philo->table->is_dead, true);
+		print_status(philo, DIED);
+	}
 	else
-		set_bool(&philo->ph_mtx, &philo->table->is_dead, false);
+		set_bool(&philo->table->read_mutex, &philo->table->is_dead, false);
+	ft_mutex(&philo->ph_mtx, UNLOCK);
 	return (get_bool(&philo->ph_mtx, &philo->table->is_dead));
 }
 
@@ -83,8 +88,6 @@ void	*monitoring(void *data)
 			if (is_philo_dead(table->philo + i) || all_philo_full(table))
 			{
 				set_bool(&table->read_mutex, &table->is_exit, true);
-				if (get_bool(&table->read_mutex, &table->is_dead) == true)
-					print_status(table->philo + i, DIED);
 				return (NULL);
 			}
 			i++;
